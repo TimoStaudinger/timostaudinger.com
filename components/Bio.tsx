acc6cd406b6de59d3b29e6ae666d92fe6490de29
Timo Staudinger <mail@timostaudinger.com>
2020-05-24 18:09:59 -0400

    Polish up homepage hero

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -2,25 +2,43 @@ import React from 'react'
 
 const Bio = () => (
   <div>
-    <h1 className="hero">
-      Hi!
-      <br />
-      I'm Timo.
-    </h1>
-    <p className="tagline">
-      I build enterprise apps for humans, not for machines.
-    </p>
+    <div className="hero">
+      <img src="/assets/profile/timo.png" className="profile" />
+      <h1 className="welcome">
+        Hi!
+        <br />
+        I'm Timo.
+      </h1>
+    </div>
+    <p className="tagline">I build enterprise apps for humans, not machines.</p>
 
     <style jsx>
       {`
         .hero {
+          display: flex;
+          flex-direction: row;
+          margin-top: 100px;
+          margin-bottom: 30px;
+          align-items: center;
+        }
+
+        .profile {
+          width: 150px;
+          height: 150px;
+          margin-right: 20px;
+        }
+
+        .welcome {
+          flex: 1;
           font-size: 3em;
           line-height: 1.5;
           font-weight: 300;
+          margin: 0;
         }
 
         .tagline {
           font-size: 1.2em;
+          margin-bottom: 60px;
         }
       `}
     </style>
