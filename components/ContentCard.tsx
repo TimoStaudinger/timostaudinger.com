628d299a50ebb0b691ca860da136adc1a5fa2d09
Timo Staudinger <mail@timostaudinger.com>
2021-02-04 10:20:28 -0500

    Fine-tune homepage styling

--- components/ContentCard.tsx
+++ components/ContentCard.tsx
@@ -61,7 +61,11 @@ const ContentCard = ({content, type}: Props) => (
     <Link href={`/${type}/[slug]`} as={`/${type}/${content.slug}`}>
       <div className="content">
         {content.previewImage ? (
-          <img src={content.previewImage} alt="preview" />
+          <img
+            className="previewImage"
+            src={content.previewImage}
+            alt="preview"
+          />
         ) : (
           <div className="placeholder">
             <div className="placeholderContent">
@@ -89,58 +93,48 @@ const ContentCard = ({content, type}: Props) => (
         background: white;
         transition: all 0.2s;
 
-        // box-shadow: 0 0 3px 1px rgba(80, 107, 135, 0.2);
-
         cursor: pointer;
         overflow: hidden;
         margin-bottom: 20px;
 
-        // -webkit-font-smoothing: subpixel-antialiased;
-        // transform: translateZ(0) translate3d(0, 0, 0) perspective(1px);
-        // backface-visibility: hidden;
-        // -webkit-perspective: 1000;
+        padding: 20px;
+        border-radius: 10px;
+
+        border: 2px solid transparent;
       }
 
       @media screen and (min-width: 500px) {
         .card {
-          flex: 0 1 calc(50% - 15px);
+          flex: 0 1 calc(50% - 64px);
         }
       }
 
       @media screen and (min-width: 700px) {
         .card {
-          flex: 0 1 calc(33% - 15px);
+          flex: 0 1 calc(33% - 64px);
         }
       }
 
       .card:hover {
-        // box-shadow: -4px 3px 8px 2px rgba(80, 107, 135, 0.5);
-        // transform: translate(2px, -2px);
-
-        box-shadow: 0 2px 4px -1px rgb(0 0 0 / 20%),
-          0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%);
+        background-color: rgb(241, 245, 248);
       }
 
       .card:focus,
       .card:focus-within {
-        // box-shadow: 0 0 4px 3px rgba(80, 107, 135, 0.3);
-        box-shadow: 0 2px 4px -1px rgb(0 0 0 / 20%),
-          0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%);
         background-color: rgb(241, 245, 248);
+        border-color: rgb(80, 107, 135);
         outline: none;
       }
 
       .title {
-        margin: 15px;
+        margin-top: 20px;
         font-weight: 700;
       }
 
       .excerpt {
-        margin-top: 20px;
+        margin-top: 10px;
         font-size: 0.9em;
         color: rgb(80, 107, 135);
-        margin: 15px;
-        margin-top: 0;
       }
 
       .placeholder {
@@ -148,6 +142,9 @@ const ContentCard = ({content, type}: Props) => (
         overflow: hidden;
         width: 150%;
         position: relative;
+        margin-left: -22px;
+        margin-right: -22px;
+        margin-top: -22px;
       }
 
       .placeholderContent {
@@ -158,8 +155,10 @@ const ContentCard = ({content, type}: Props) => (
         color: rgb(218, 228, 237);
       }
 
-      .placeholderIcon {
-        margin: 2px;
+      .previewImage {
+        margin-left: -22px;
+        margin-right: -22px;
+        margin-top: -22px;
       }
     `}</style>
   </div>
